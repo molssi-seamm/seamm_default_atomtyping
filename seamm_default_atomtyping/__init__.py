a06fbf2477294b7f1df54b57b3d380f11f8fc584
Eliseo Marin <meliseo@vt.edu>
2021-04-07 17:35:15 -0400

    Remove old modules

--- seamm_default_atomtyping/__init__.py
+++ seamm_default_atomtyping/__init__.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """
-seamm_default_atomtyping
-A plug-in that implements the default atomtyping method in SEAMM
+seamm_default_atomtypingyping
+A plug-in that implements the default atomtypingyping method in SEAMM
 """
 
 # Bring up the classes so that they appear to be directly in
-# the seamm_default_atomtyping package.
+# the seamm_default_atomtypingyping package.
 
-from seamm_default_atomtyping.seamm_default_atomt import SeammDefaultAtomtyping  # noqa: F401, E501
-from seamm_default_atomtyping.seamm_default_atomt_parameters import SeammDefaultAtomtypingParameters  # noqa: F401, E501
-from seamm_default_atomtyping.seamm_default_atomt_step import SeammDefaultAtomtypingStep  # noqa: F401, E501
-from seamm_default_atomtyping.tk_seamm_default_atomt import TkSeammDefaultAtomtyping  # noqa: F401, E501
+from seamm_default_atomtyping.seamm_default_atomtyping import SeammDefaultAtomtyping  # noqa: F401, E501
+from seamm_default_atomtyping.seamm_default_atomtyping_step import SeammDefaultAtomtypingStep  # noqa: F401, E501
 
 # Handle versioneer
 from ._version import get_versions
